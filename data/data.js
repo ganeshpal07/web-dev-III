162a6af98201eb41478a06b5477485663a1e29f0
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-12 14:04:31 +0530

    add gitignore

---
+++ data/data.js
@@ -0,0 +1,7 @@
+const cities = [
+  { name: "New York", population: 8336000 },
+  { name: "Los Angeles", population: 3979000 },
+  { name: "Chicago", population: 2705000 }
+];
+
+module.exports = cities;
