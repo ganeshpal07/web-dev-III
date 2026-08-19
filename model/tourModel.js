4e23a5a3fa5c4d78c187bcc7cdd92b6059887c34
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-19 14:05:48 +0530

    initial commit

---
+++ model/tourModel.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const toursFilePath = path.join(__dirname, '../data/tours.json');
+
+const getAllTours = () => {
+  const toursData = fs.readFileSync(toursFilePath, 'utf-8');
+  return JSON.parse(toursData);
+};
+
+const getTourById = (id) => {
+  const toursData = getAllTours();
+  return toursData.find(tour => tour.id === id);
+}
+
+module.exports = {
+  getAllTours,
+  getTourById
+};
